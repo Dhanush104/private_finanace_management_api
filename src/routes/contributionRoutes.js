0cd595059d4de6815df2f7d6d1e85a2e36bf6ce5
Dhanush@104 <trdhanush1234@gmail.com>
2026-03-01 16:11:09 +0530

    removing incoming changes from main branches 3

--- src/routes/contributionRoutes.js
+++ src/routes/contributionRoutes.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
-<<<<<<< HEAD
 const { getContributions, recordContribution, updateContribution, approveContribution, rejectContribution } = require('../controllers/contributionController');
-=======
-const { getContributions, recordContribution, updateContribution } = require('../controllers/contributionController');
->>>>>>> main
+
 const { protect } = require('../middleware/auth');
 const { authorize } = require('../middleware/authorize');
 const { validate } = require('../middleware/validate');
@@ -13,10 +10,9 @@ router.use(protect);
 router.get('/', getContributions);
 router.post('/', validate(contributionSchema), recordContribution);
 router.put('/:id', authorize('admin'), validate(updateContributionSchema), updateContribution);
-<<<<<<< HEAD
+
 router.post('/:id/approve', authorize('admin'), approveContribution);
 router.post('/:id/reject', authorize('admin'), rejectContribution);
-=======
->>>>>>> main
+
 
 module.exports = router;
